be35ca2755bcd162e0e7cf54e372ebb5568f8864
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-03-01 21:36:34 -0500

    Se actualizó la clase correo

--- src/persistencia/Correo.java
+++ src/persistencia/Correo.java
@@ -18,37 +18,77 @@ import javax.mail.internet.MimeMessage;
  */
 public class Correo {
     
-    public void enviarCorreo(String correo, String nombre, String apellido){
-        
+   public void enviarCorreo(String correo, String nombre, String apellido) {
+
         Properties propiedad = new Properties();
         propiedad.setProperty("mail.smtp.host", "smtp.gmail.com");
         propiedad.setProperty("mail.smtp.starttls.enable", "true");
         propiedad.setProperty("mail.smtp.port", "587");
         propiedad.setProperty("mail.smtp.auth", "true");
-        
+
         Session sesion = Session.getDefaultInstance(propiedad);
         String correoEnvia = "correodeprueba8771@gmail.com";
         String contrasena = "estudiantes1234";
         String receptor = correo;
         String asunto = "Bienvenido a Fundamentos de Bases de Datos!";
-        String mensaje="Bienvenido "+nombre+" "+apellido+" su registro fue satisfactorio.";
-        
+        String mensaje = "Bienvenido " + nombre + " " + apellido + " su registro fue satisfactorio.";
+
         MimeMessage mail = new MimeMessage(sesion);
         try {
-            mail.setFrom(new InternetAddress (correoEnvia));
-            mail.addRecipient(Message.RecipientType.TO, new InternetAddress (receptor));
+            mail.setFrom(new InternetAddress(correoEnvia));
+            mail.addRecipient(Message.RecipientType.TO, new InternetAddress(receptor));
             mail.setSubject(asunto);
             mail.setText(mensaje);
-            
+
             Transport transportar = sesion.getTransport("smtp");
-            transportar.connect(correoEnvia,contrasena);
-            transportar.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));          
-            transportar.close();   
-            
+            transportar.connect(correoEnvia, contrasena);
+            transportar.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+            transportar.close();
+
         } catch (AddressException ex) {
             Logger.getLogger(Panel.class.getName()).log(Level.SEVERE, null, ex);
         } catch (MessagingException ex) {
             Logger.getLogger(Panel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    public void enviarCorreo(String correo, String usuario, String valor, String[] datos) {
+        Properties propiedad = new Properties();
+        propiedad.setProperty("mail.smtp.host", "smtp.gmail.com");
+        propiedad.setProperty("mail.smtp.starttls.enable", "true");
+        propiedad.setProperty("mail.smtp.port", "587");
+        propiedad.setProperty("mail.smtp.auth", "true");
+
+        Session sesion = Session.getDefaultInstance(propiedad);
+        String correoEnvia = "correodeprueba8771@gmail.com";
+        String contrasena = "estudiantes1234";
+        String receptor = correo;
+        String asunto = "Confirmación solicitud prestamo";
+        String mensaje = "Señor@ " + usuario + ", se confirmó el prestamo de las siguientes peliculas a su nombre: \n";
+
+        for (int i = 0; i < datos.length; i++) {
+            mensaje +=  (i+1) + ": " + datos[i] + "\n";
+        }
+        
+        mensaje += "\n Con un costo total a pagar de: " + valor;
+
+        MimeMessage mail = new MimeMessage(sesion);
+        try {
+            mail.setFrom(new InternetAddress(correoEnvia));
+            mail.addRecipient(Message.RecipientType.TO, new InternetAddress(receptor));
+            mail.setSubject(asunto);
+            mail.setText(mensaje);
+
+            Transport transportar = sesion.getTransport("smtp");
+            transportar.connect(correoEnvia, contrasena);
+            transportar.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+            transportar.close();
+
+        } catch (AddressException ex) {
+            Logger.getLogger(Panel.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MessagingException ex) {
+            Logger.getLogger(Panel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
