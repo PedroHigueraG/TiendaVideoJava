d3ecaa2ca7ef0555aa7779265606337c4f9ae6bf
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-03-01 21:30:28 -0500

    Se actualizó la clase prestamo

--- src/negocio/Prestamo.java
+++ src/negocio/Prestamo.java
@@ -2,6 +2,7 @@ package negocio;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import persistencia.BasesDatos;
 
@@ -10,13 +11,16 @@ import persistencia.BasesDatos;
  * @author Cristian Meneses y Pedro Higuera
  */
 public class Prestamo {
-    
-     private ArrayList lista;
+
+    private ArrayList lista;
     BasesDatos bd;
 
     public void crearLista() {
         if (lista == null) {
             lista = new ArrayList();
+        }
+        if (bd == null) {
+            bd = new BasesDatos();
         }
     }
 
@@ -28,25 +32,30 @@ public class Prestamo {
         if (dato.equals("1") || dato.equals("2") || dato.equals("3") || dato.equals("4")
                 || dato.equals("5") || dato.equals("6") || dato.equals("7") || dato.equals("8")
                 || dato.equals("9") || dato.equals("10")) {
-            agregarLista(dato);
-            return true;
+            if (bd.consultarEstado(dato)) {
+                agregarLista(dato);
+                return true;
+            } else {
+                JOptionPane.showMessageDialog(null, "pelicula no disponible, seleccione otra");
+                return false;
+            }
         } else {
             return false;
         }
     }
-    
-    public void llenarTabla(JTable tabla){
-        bd = new BasesDatos();
-        bd.consultarPrestamo(lista,tabla);
+
+    public void llenarTabla(JTable tabla) {
+        bd.consultarPrestamo(lista, tabla);
     }
-    
-    public void mostrarLista() {
-        for (Iterator iterator = lista.iterator(); iterator.hasNext();) {
-            Object next = iterator.next();
-            System.out.println(next);
-            
-        }
-        
+
+    public void limpiar() {
+        bd.actualizarEstado(lista);
+        lista.clear();
     }
-    
+
+    public ArrayList getLista() {
+        return lista;
+    }
+
 }
+
