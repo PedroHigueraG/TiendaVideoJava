27a3acc609fa0647dbf62013025ad57c841b8d21
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-03-01 21:29:06 -0500

    Se actualizó el controlador

--- src/negocio/Controlador.java
+++ src/negocio/Controlador.java
@@ -14,20 +14,21 @@ import presentacion.VentanaPrincipal;
  */
 public class Controlador implements ActionListener {
 
-    //ATRIBUTOS
+       //ATRIBUTOS
     private String nombre, apellido, correo, telefono, direccion, credito;
     private int id;
     VentanaPrincipal ventana;
     BasesDatos bd;
     Correo mail;
-
+    Prestamo pres;
 
     //CONTROLADOR
-    public Controlador(VentanaPrincipal vista,BasesDatos base, Correo correo){
-        
-       ventana = vista;
+    public Controlador(VentanaPrincipal vista, BasesDatos base, Correo correo) {
+
+        ventana = vista;
         bd = base;
         mail = correo;
+        pres = new Prestamo();
 
         ventana.pRegistro.getRegistrarse().addActionListener(this);
         ventana.pRegistro.getAtras().addActionListener(this);
@@ -46,8 +47,7 @@ public class Controlador implements ActionListener {
         bd.consultarVacios();
     }
 
-
-    public void limpiarCampos(){
+    public void limpiarCampos() {
         ventana.pRegistro.getNombre().setText("");
         ventana.pRegistro.getApellido().setText("");
         ventana.pRegistro.getCorreo().setText("");
@@ -71,7 +71,6 @@ public class Controlador implements ActionListener {
             } else {
                 int cred = Integer.parseInt(credito);
                 int tel = Integer.parseInt(telefono);
-                System.out.println(tel);
                 id = bd.consultarId();
                 id++;
                 bd.AgregarUsuario(id, nombre, apellido, tel, direccion, cred, correo);
@@ -101,11 +100,10 @@ public class Controlador implements ActionListener {
             String user = ventana.pIngreso.getUsuario().getText();
             String contrasena = new String(ventana.pIngreso.getContraseña().getPassword());
 
-            boolean usuario = bd.consultarEmpleado(" "+user," "+contrasena);
-            System.out.println(usuario);
+            boolean usuario = bd.consultarEmpleado(" " + user, " " + contrasena);
 
             if (usuario) {
-                JOptionPane.showMessageDialog(ventana, "bienvenido " + bd.consultarNombre(" "+contrasena));
+                JOptionPane.showMessageDialog(ventana, "bienvenido " + bd.consultarNombre(" " + contrasena));
                 ventana.pBusqueda.setVisible(true);
                 ventana.pIngreso.setVisible(false);
             } else {
@@ -115,35 +113,49 @@ public class Controlador implements ActionListener {
             }
         }
         if (ae.getSource() == ventana.pBusqueda.getBuscar()) {
-            String op = (String)ventana.pBusqueda.getOpciones().getSelectedItem();
+            String op = (String) ventana.pBusqueda.getOpciones().getSelectedItem();
             String titulo = ventana.pBusqueda.getBusqueda().getText();
-            String estado="";
-            if(ventana.pBusqueda.getLibre().isSelected()){
-                estado=" L";
-            }if(ventana.pBusqueda.getPrestada().isSelected()){
-                estado=" P";
-            }if(ventana.pBusqueda.getDañada().isSelected()){
-                estado=" D";
-            }if(ventana.pBusqueda.getPerdida().isSelected()){
-                estado=" PP";
+            String estado = "";
+            if (ventana.pBusqueda.getLibre().isSelected()) {
+                estado = " L";
             }
-            if("Titulo".equals(op)){
-                bd.consultarPelicula(ventana.pBusqueda.getTabla(),titulo,estado);
+            if (ventana.pBusqueda.getPrestada().isSelected()) {
+                estado = " P";
             }
-            if("Actor".equals(op)){
-                bd.consultarActor(ventana.pBusqueda.getTabla(),titulo,estado);
+            if (ventana.pBusqueda.getDañada().isSelected()) {
+                estado = " D";
             }
-            if("Membresia".equals(op)){
+            if (ventana.pBusqueda.getPerdida().isSelected()) {
+                estado = " PP";
+            }
+            if ("Titulo".equals(op)) {
+                bd.consultarPelicula(ventana.pBusqueda.getTabla(), titulo, estado);
+            }
+            if ("Actor".equals(op)) {
+                bd.consultarActor(ventana.pBusqueda.getTabla(), titulo, estado);
+            }
+            if ("Membresia".equals(op)) {
                 bd.consultarMembresia(ventana.pBusqueda.getTabla(), titulo);
             }
         }
         if (ae.getSource() == ventana.pBusqueda.getSiguiente()) {
-           bd.consultarPeliculaLibre(ventana.pPrestamo.getTabla());
+            pres.llenarTabla(ventana.pPrestamo.getTabla());
             ventana.pBusqueda.setVisible(false);
             ventana.pPrestamo.setVisible(true);
         }
         if (ae.getSource() == ventana.pBusqueda.getAgregar()) {
-            System.out.println("ola");
+            if (ventana.pBusqueda.getId().getText().equals("")) {
+                JOptionPane.showMessageDialog(ventana, "por favor ingrese un id para poder seleccionar");
+            } else {
+                pres.crearLista();
+                if (!pres.validarDato(ventana.pBusqueda.getId().getText())) {
+                    JOptionPane.showMessageDialog(ventana, "id no valido, seleccione uno entre 1 y 10");
+                } else {
+                    JOptionPane.showMessageDialog(ventana, "agregado");
+                    ventana.pBusqueda.getId().setText("");
+                }
+            }
+
         }
         if (ae.getSource() == ventana.pBusqueda.getAtras()) {
             ventana.pBusqueda.setVisible(false);
@@ -156,6 +168,36 @@ public class Controlador implements ActionListener {
         if (ae.getSource() == ventana.pInicial.getSalir()) {
             bd.DetenerConexion();
             System.exit(0);
+        }
+        if (ae.getSource() == ventana.pPrestamo.getGuardar()) {
+            String valor = ventana.pPrestamo.getTotal().getText();
+            String usuario = " " + ventana.pPrestamo.getUsuario().getText();
+            String datos[] = new String[pres.getLista().size()];
+
+            for (int i = 0; i < pres.getLista().size(); i++) {
+                datos[i] = (String) ventana.pPrestamo.getTabla().getValueAt(i, 1);
+            }
+
+            boolean res = bd.consultarUsuario(usuario);
+            if (res) {
+                String correo = bd.consultarCorreo(usuario);
+                mail.enviarCorreo(correo, usuario, valor, datos);
+                ventana.pPrestamo.getTotal().setText("");
+                ventana.pPrestamo.getUsuario().setText("");
+                ventana.pPrestamo.getTabla().removeAll();
+                pres.limpiar();
+                
+                int idprestamo = bd.consultarIdPrestamo() + 1;
+                int iduser = bd.consultarIdUsuario(usuario);
+                int idemp = bd.consultarIdEmpleado(bd.getUsuarioEmpleado());
+                bd.llenarPrestamo(idprestamo,iduser,idemp);
+                
+                int item = bd.consultarIdDetallePrestamo() +1;
+                int costo = Integer.parseInt(valor);
+                bd.llenarDetallePrestamo(item, idprestamo, 2, costo);
+            } else {
+                JOptionPane.showMessageDialog(ventana, "no se ha encontrado ese nombre de usuario");
+            }
         }
     }
 
